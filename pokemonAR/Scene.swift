ac716a9fa50861e0f662185e093432d276dc3ca3
sergioeabarcaf <sergioaf1991@gmail.com>
2018-07-10 07:37:25 -0400

    creación de SKLabelNode con el contador de targets

--- pokemonAR/Scene.swift
+++ pokemonAR/Scene.swift
@@ -11,8 +11,23 @@ import ARKit
 
 class Scene: SKScene {
     
+    let remainingLabel = SKLabelNode()
+    var timer : Timer?
+    var targetsCreated = 0
+    var targetsCount = 0 {
+        didSet{
+            self.remainingLabel.text = "Faltan: \(targetsCount)"
+        }
+    }
+    
     override func didMove(to view: SKView) {
         // Setup your scene here
+        remainingLabel.fontSize = 30
+        remainingLabel.fontName = "Avenir Next"
+        remainingLabel.color = .white
+        remainingLabel.position = CGPoint(x: 0, y: view.frame.midY-50)
+        addChild(remainingLabel)
+        targetsCount = 0
     }
     
     override func update(_ currentTime: TimeInterval) {
